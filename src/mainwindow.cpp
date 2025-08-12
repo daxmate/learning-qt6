38ac62ebaef67492f499f64f9b0864d371404ed2
dax <dax26d@gmail.com>
2025-08-12 17:58:30 +0800

    修改一点小风险

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -9,7 +9,8 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
                                           ui(std::make_unique<Ui::MainWindow>()),
                                           lessonData(QSharedPointer<QStringList>::create()) {
     ui->setupUi(this);
-    lessonData->append("hellworld");
+    lessonData->append("hello_world");
     lessonModel = new LessonModel(lessonData, this);
+    Q_ASSERT(ui->listView != nullptr);
     ui->listView->setModel(lessonModel);
 }
